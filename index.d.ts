5a806305fb420981327890cfa3007a0af98d5fac
noahgreff <greffnoah@gmail.com>
2023-08-01 15:37:12 +0200

    updated types

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,35 @@
-export function read(): string
+import { EventEmitter } from 'events'
 
-export function write(data: string): void
+export interface ClipboardOptions {
+	interval?: number
+	captureRejections?: boolean // EventEmitterOptions options
+}
 
-export function readFiles(): string[]
+export class Clipboard extends EventEmitter {
+	constructor(options?: ClipboardOptions)
 
-export function writeFiles(paths: string[]): void
+	read(): string
+	write(text: string): void
+	readFiles(): string[]
+	writeFiles(paths: string[]): void
+	clear(): void
+	stop(): void
 
-export function clear(): string
+	on(event: 'text-changed', listener: (text: string) => void): this
+	on(event: 'file-changed', listener: (paths: string[]) => void): this
+
+	once(event: 'text-changed', listener: (text: string) => void): this
+	once(event: 'file-changed', listener: (paths: string[]) => void): this
+
+	addListener(event: 'text-changed', listener: (text: string) => void): this
+	addListener(event: 'file-changed', listener: (paths: string[]) => void): this
+
+	removeListener(event: 'text-changed', listener: (text: string) => void): this
+	removeListener(event: 'file-changed', listener: (paths: string[]) => void): this
+	removeAllListeners(event?: 'text-changed' | 'file-changed'): this
+
+	listeners(event: 'text-changed' | 'file-changed'): ((text: string) => void)[]
+
+	emit(event: 'text-changed', text: string): boolean
+	emit(event: 'file-changed', paths: string[]): boolean
+}
